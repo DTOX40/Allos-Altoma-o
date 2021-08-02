b6975b52d10f194ece9cbf17781d4c4067528f75
Daniel Candido <daniel.csilva@castgroup.com>
2021-08-02 13:24:15 -0300

    criação do controller home e atalhos da navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get '/home', to: 'home#index'
   resources :tipo_produtos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
