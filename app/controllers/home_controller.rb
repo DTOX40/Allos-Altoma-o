c53a70bf78175ccbeea50d758130e48e822e0abd
Daniel Candido <daniel.csilva@castgroup.com>
2021-08-03 15:51:07 -0300

    criando do lyout do site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
     skip_before_action :valida_logado_admin
     layout "site"
-    def index;end
+
+    def index
+      @produtos = Produto.all
+    end
 end
