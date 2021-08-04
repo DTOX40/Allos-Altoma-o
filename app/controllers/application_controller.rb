c8db519c3a338fd6fad90ef17a56fd87dd7c5019
Daniel Candido <daniel.csilva@castgroup.com>
2021-08-03 23:43:35 -0300

    criar usuario admim rolbac

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
- 
+    before_action :valida_logado_admin
+
+    def valida_logado_admin
+      if cookies[:allos_sistemas_admin].present?
+        hash_admin = JSON.parse(cookies[:allos_sistemas_admin])
+      if hash_admin["id"].present?
+        administradores = Administrador.where(id: hash_admin["id"]) 
+        if administradores.count > 0 
+          @administrador = administradores.first
+          return 
+        end  
+       end
+     end        
+       redirect_to "/login"     
+    end   
 end
